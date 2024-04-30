e92bf5dea44a616b8accdda664d2faf19ba1441c
Pamela Viale <pamelaviale87@gmail.com>
2024-04-30 16:45:14 -0300

    agregue media querys para tablet y escritorio

--- recetas-dulces/script.js
+++
@@ -1,8 +0,0 @@
-// Obtener el elemento del menú
-const menu = document.querySelector('.container-menu');
-
-// Agregar un evento de clic al elemento del menú
-menu.addEventListener('click', function() {
-    // Alternar la clase 'clicked' en el menú cuando se hace clic en él
-    this.classList.toggle('clicked');
-});
